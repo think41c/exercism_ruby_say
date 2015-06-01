55dbcf98ca428335d59ad9a4247d193421fc5718
Tony DiNitto <think41c@gmail.com>
2015-05-31 22:43:41 -0500

    All numbers listed out for the ones place.

--- say.rb
+++ say.rb
@@ -1,7 +1,11 @@
 class Say
   def initialize(number)
-    @spelled_out = { 0 => "zero", 1 => "one", 
-                    2 => "two", 3 => "three"
+    @spelled_out = { 0  => "zero",  1 => "one", 
+                     2  => "two",   3 => "three",
+                     4  => "four",  5 => "five",
+                     6  => "six",   7 => "seven",
+                     8  => "eight", 9 => "nine",
+                     10 => "ten" 
                    }
     @number = number
   end
