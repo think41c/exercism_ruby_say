fddf6ae44e21e03d35c95daae2b080b8f5ae8604
Tony DiNitto <think41c@gmail.com>
2015-06-01 22:08:39 -0500

    Pseudocode added.

--- say.rb
+++ say.rb
@@ -3,27 +3,43 @@ class Say
     # Things don't become uniform and totally systematic until
     # the number 20. 
 
-    @ones_place  = { 0  => "zero",  1 => "one", 
-                     2  => "two",   3 => "three",
-                     4  => "four",  5 => "five",
-                     6  => "six",   7 => "seven",
-                     8  => "eight", 9 => "nine",
-                     10 => "ten",  11 => "eleven",
-                     12 => "twelve", 13 => "thirteen",
-                     14 => "fourteen", 15 => "fifteen",
-                     16 => "sixteen", 17 => "seventeen",
-                     18 => "eighteen", 19 => "nineteen"
-                   }
-    @tens_place = {2 => "twenty", 3 => "thirty",
-                   4 => "fourty", 5 => "fifty",
-                   6 => "sixty"
+    @ones_place = { 
+                  0  => "zero",  1 => "one", 
+                  2  => "two",   3 => "three",
+                  4  => "four",  5 => "five",
+                  6  => "six",   7 => "seven",
+                  8  => "eight", 9 => "nine",
+                  10 => "ten",  11 => "eleven",
+                  12 => "twelve", 13 => "thirteen",
+                  14 => "fourteen", 15 => "fifteen",
+                  16 => "sixteen", 17 => "seventeen",
+                  18 => "eighteen", 19 => "nineteen"
+                  }
+    @tens_place = { 
+                  2 => "twenty", 3 => "thirty",
+                  4 => "fourty", 5 => "fifty",
+                  6 => "sixty", 7 => "seventy",
+                  8 => "eigthy", 9 => "ninety"
                   }
 
     @number = number
   end
 
   def in_english
-    if @number < 19 
+    # Pseudo-code
+    # 100,456
+    # Make the number into an array of 3 digit Fixnums.
+    # [100,456]
+    # If array length is 2, append "thousands".
+    # If array length is 3, append "millions". 
+    # If array length is 4, append "billions".
+    # Work from first element in array to end.
+    # 100 - @ones_place = 1 + "hundred"
+    # If next digit is 0, then move to next.
+    # 456 - @ones_place = 4 + "hundred"
+    # Then run algorithm for 0-99. "fifty six"
+
+    if @number < 19
       result = @ones_place[@number]
     elsif @number.to_s[-1] == "0"
       result = @tens_place[@number.to_s[-2].to_i]
@@ -33,4 +49,4 @@ class Say
     result
   end
 end
-p Say.new(22).in_english
+p Say.new(52).in_english
