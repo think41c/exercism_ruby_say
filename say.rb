bb5196e0f7673a129c8cb785dd233bdb7bb27791
Tony DiNitto <think41c@gmail.com>
2015-07-04 17:22:07 -0500

    Mild refactor.

--- say.rb
+++ say.rb
@@ -1,6 +1,7 @@
 class Say
   def initialize(number)
-    @ones_place = {""  => "",         "00" => "", 
+    @ones_place = {
+                           "00" => "", 
                   "0"  => "zero",      "1" => "one", 
                   "2"  => "two",       "3" => "three",
                   "4"  => "four",      "5" => "five",
